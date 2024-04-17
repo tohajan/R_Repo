421e78f4407b44f26248746dfbf5d7fd3587140a
tohajan <tohajanlillah@gmail.com>
2024-04-17 10:43:32 -0400

    update "file conversions.R"

--- file conversions.R
+++ file conversions.R
@@ -1,5 +1,8 @@
 library(knitr)
 
-purl("Data Handling & Graphics.Rmd")
+# purl("Data Handling & Graphics.Rmd") # extracts only R codes from the Rmd file
 
+
+# purl("Data Handling & Graphics.Rmd", output = "Data Handling + Graphics.R", documentation = 2) 
+# extracts both R codes and documentation
 
