9a1d97c1a001ad444e41a5252053d5671811bad8
tohajan <tohajanlillah@gmail.com>
2024-04-17 14:23:52 -0400

    make changes to the file

--- file conversions.R
+++ file conversions.R
@@ -1,8 +1,15 @@
+
+# 1. CONVERT .Rmd FILE TO .R FILE
+
 library(knitr)
+# a. To extract only R codes from the Rmd file:
+# purl("Data Handling & Graphics.Rmd") # the "purl" function is located in the "knitr" library
 
-# purl("Data Handling & Graphics.Rmd") # extracts only R codes from the Rmd file
-
-
+# b. To extract R codes along with documentation (i.e., the non-codes in an .Rmd file):
 # purl("Data Handling & Graphics.Rmd", output = "Data Handling + Graphics.R", documentation = 2) 
-# extracts both R codes and documentation
 
+
+# 2. MAKE AN .Rmd DOC RUN IN GITHUB (see complete details 
+# at https://gist.github.com/JoshuaTPierce/b919168421b40e06481080eb53c3fb2f):
+# In the markdown document, change "output=html_document" to "output=github_document"
+# The above change will create a .md document which renders in github just like a normal .Rmd file in R
