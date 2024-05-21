c148a35e4774486040c05bbce325b8e583165b65
tohajan <tohajanlillah@gmail.com>
2024-05-20 22:32:43 -0400

    complete making changes/updates

--- Simulation.R
+++ Simulation.R
@@ -176,38 +176,39 @@ plot(0:(generations - 1), xt_1, type = "o", xlim = c(200, 250), xlab = "Generati
 plot(0:(generations - 1), xt_2, type = "o", xlim = c(200, 250), xlab = "Generations", ylab = "population density", main = "r=3.5")
 plot(0:(generations - 1), xt_3, type = "o", xlim = c(200, 250), xlab = "Generations", ylab = "population density", main = "r=3.9")
 
-#' 
 #' Note that the graph becomes more "chaotic" as r increases
 #' 
-#' **Note to self**: The plots didn't display in VSCode. Running the code ".vsc.attach()" still didn't fix it. (It worked in an earlier work though)
+#' **Note to self**: The plots didn't display when script is executed in VSCode. Running the 
+#' code ".vsc.attach()" fixed the issue.
 #' 
 #' 
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #' ## Bifurcation Diagrams
-#' A bifurcation diagram can help visualize how different patterns of growth appear in populations over periods
-## ------------------------------------------------------------------------------------------------
+#' A bifurcation diagram can help visualize how different patterns of growth appear in 
+#' populations over periods
 minr <- 2.5 # minimum r
 maxr <- 4.0 # maximum r
 inc <- 0.01 # incremental unit
 rd <- seq(minr, maxr, inc)
 
+#' In bifurcation diagrams, the interest is usually focused on later generations because 
+#'...unpredictable patterns generally appear after the population has attained equilibrium.
+ttransients <- 100 # "transient" is a term referring to the early periods (generations) in a 
+#...population before it reaches equilibrium. In the current example, transient is set to 100, 
+#...i.e., the diagram should ignore the 1st 100 generations of the population
 #' 
+trange <- 500 # the data will contain the last 400 generations (i.e, after the 1st 100, i.e., it 
+#...will display only generations 101-500)
 #' 
-#' 
-#' In bifurcation diagrams, the interest is usually focused on later generation because unpredictable patterns generally appear after the population has attained equilibrium.
-## ------------------------------------------------------------------------------------------------
-ttransients <- 100 # "transient" is a term referring to the early periods (generations) in a population before it reaches equilibrium. In the current example, transient is set to 100, i.e., the diagram should ignore the 1st 100 generations of the population
-
-#' 
-## ------------------------------------------------------------------------------------------------
-trange <- 400 # the data will contain 400 generations (i.e, after the 1st 100, i.e., it will display only generations 101-500)
-
-#' 
-#' 
-## ------------------------------------------------------------------------------------------------
 #create an empty plot:
-par(mfrow = c(1, 1)) # This resets the plot (from a previous code that demarcates the plot for three objects). Note that this is only needed in a basic R environment, not in R Markdown (where codes are organized in chunks, such that restrictions/definitions in a previous plot wouldn't carry over to a next one)
+par(mfrow = c(1, 1)) # This resets the plot (from a previous code that demarcates the plot for 
+#...three objects). NB: this is only needed in a basic R environment, not in R Markdown (where 
+#...codes are organized in chunks, such that restrictions/definitions in a previous plot 
+#...doesn't carry over to a next one)
 
-plot(c(minr, maxr), c(0, 1), type = "n", pch = ".", xlab = "r", ylab = "Population density in the last 400 generations")
+plot(c(minr, maxr), c(0, 1), type = "n", pch = ".", xlab = "r", 
+     ylab = "Population density in the last 400 generations")
 
 # Now insert the plot:
 for (r in rd) {
@@ -215,79 +216,83 @@ for (r in rd) {
     for (i in 1:ttransients) {
         x <- r * x * (1 - x)
     }
-    for (i in 1:trange) {
+    for (i in 101:trange) {
         x <- r * x * (1 - x)
         points(r, x, pch = ".")
     }
 } # recall that rd, ttransients, and trange have been defined previously
 
+#' PS: Notice the different point cycles (1, 2, 4, etc...) in the resulting bifurcation diagram
+
 #' 
 #' 
-#' *Note to self: I wonder why it is not "101:trange" since it's a continuation from ttransients. Same result either way, though.*
-#' 
-#' Notice the different point cycles (1, 2, 4, etc...) in the resulting bifurcation diagram
 #' 
 #' 
+#' 
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #' # **DIFFERENTIAL EQUATIONS IN R**
-#' Differential equations are used for representing population dynamic when the population changes continuously (unlike at discrete time intervals)
+#' Differential equations are used for representing population dynamic when the population 
+#' ..changes continuously (unlike at discrete time intervals)
 #' 
 #' ## Representing Basic Dynamics
-## ------------------------------------------------------------------------------------------------
 # install.packages("deSolve")
-
 #' 
-#' The ode (ordinary differential equation) function used later is contained in the "deSolve" package. Some of the function's arguments are:\
-#' y: the state variable(s), y[1] is the 1st state var, y[2] is 2nd, etc.\
+#' The ode (ordinary differential equation) function used below is contained in the "deSolve" 
+#' ..package. Some of the function's arguments are:
+#' y: the state variable(s), y[1] is the 1st state var, y[2] is 2nd, etc.
 #' times: sequence of output\
-#' func: the set of derivatives; defined as function(times, y, parms)\
-#' parms: parameters (constant) passed, p[1] is the 1st parameter\
+#' func: the set of derivatives; defined as function(times, y, parms)
+#' parms: parameters (constant) passed, p[1] is the 1st parameter
 #' method: numerical integration algorithm e.g., Runge-Kutta (default is Isoda)
-## ------------------------------------------------------------------------------------------------
 library(deSolve)
-
 #' 
-## ------------------------------------------------------------------------------------------------
 cgrowth <- function(times, y, parms) {
     dN_dT <- p[1] * y[1]
     return(list(dN_dT))
 }
 
 #' 
-## ------------------------------------------------------------------------------------------------
 p <- 0.5
 y0 <- 2
 t <- 0:20
 sol <- ode(y = y0, times = t, func = cgrowth, parms = p) # ode = ordinary differential equation
-
+(sol)
 #' 
-#' 
-## ------------------------------------------------------------------------------------------------
 plot(t, sol[, 2], type = "o")
 
 #' 
-#' ## Continous time logistic equation
-#' The above problem can also be solved using the continous time logistic equation (i.e., dN/dT = rN (1 - N/k):
-## ------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+#' ## Continuous time logistic equation
+#' The above problem can also be solved using the continuous time logistic equation (i.e., 
+#' dN/dT = rN (1 - N/k):
 clogistic <- function(times, y, parms) {
     dN_dT <- p[1] * y[1] * (1 - (y[1] / p[2]))
     return(list(dN_dT))
 }
-
 #' 
-## ------------------------------------------------------------------------------------------------
 p <- c(0.5, 1000)
 y0 <- 2
 t <- 0:30
 sol2 <- ode(y = y0, times = t, func = clogistic, parms = p)
+sol2
 plot(t, sol2[, 2], type = "o")
-# Note the sigmoidal shape of the curve, which is xteristic of a logistic growth
+# Note the sigmoidal shape of the curve, which is characteristic of a logistic growth
 
 #' 
 #' 
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #' ## Lotka-Volterra Predation Equations
-#' Instantaneous rate of change in the prey population is given as: dN/dT = (r * N) - a * P * N \
-#' Instantaneous rate of change in the predator popn is given as: dP/dT = -(b * P) + f * P * N
-## ------------------------------------------------------------------------------------------------
+#' Instantaneous rate of change in the prey population is given as: 
+#'        dN/dT = (r * N) - a * P * N
+#' Instantaneous rate of change in the predator popn is given as: 
+#'        dP/dT = -(b * P) + f * P * N
 library(lattice)
 r <- 0.5; a <- 0.01; f <- 0.01; b <- 0.2
 p <- c(r, a, b, f)
@@ -295,7 +300,6 @@ y0 <- c(N = 25, P = 5)
 t <- seq(0, 200, 0.1)
 
 #' 
-## ------------------------------------------------------------------------------------------------
 pred_prey <- function(times, y, parms) {
     N <- y[1]
     P <- y[2]
@@ -306,11 +310,9 @@ pred_prey <- function(times, y, parms) {
     }
     )
 }
-
 #' 
-#' 
-## ------------------------------------------------------------------------------------------------
 output <- ode(y = y0, times = t, func = pred_prey, parms = p)
+output[1:10, ] # first 10 observations/rows/elements in the object "output"
 par(mfrow = c(2, 1))
 matplot(output[, 1], output[, 2:3], type = "o", xlab = "Time??",ylab = "Population Size")
 
@@ -320,81 +322,84 @@ plot(output[, 2], output[, 3], type = "l", xlab = "Prey density", ylab = "Predat
 #' 
 #' 
 #' 
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #' # **STOCHASTIC MODELS**
-#' Stochastic models deal with random variables/numbers. Random numbers can be generated by using any of two procedures: \
-#' 1. By drawing from existing distributions \
+#' Stochastic models deal with random variables/numbers. Random numbers can be generated by 
+#' ...using any of two procedures:
+#' 1. By drawing from existing distributions
 #' 2. By drawing from custom-made selections
 #' 
-#' ## 1. Using Exisitng Distributions
+#' ## 1. Using Existing Distributions
 #' ### A. Uniform Distribution -- the general form of the function is: \
-#' runif(n, min, max)
+#'        runif(n, min, max)
 #' 
-## ------------------------------------------------------------------------------------------------
-runif(15, 1, 10) # generate 15 random numbers btw 1 and 10
-
+runif(15, 1, 10) # generate 15 random numbers of uniform distribution btw 1 and 10
 #' 
 #' If the min and max are not specified, R sets the default to 0 and 1 respectively:
-## ------------------------------------------------------------------------------------------------
-runif(15) #15 random numbers btw 0 and 1
+runif(15) #15 random numbers of uniform distribution btw 0 and 1
 
 #' 
 #' ### B. Normal (or Gaussian) Distribution
-#' This distribution takes the shape of a bell curve. The general form of its function is:\
-#' rnorm(n, mean, sd)
-## ------------------------------------------------------------------------------------------------
+#' This distribution takes the shape of a bell curve. The general form of its function is:
+#'        rnorm(n, mean, sd)
 rnorm(10, 50, 100)
 
 #' 
 #' As with runif(), the mean and sd has a default value of 0 and 1 respectively:
-## ------------------------------------------------------------------------------------------------
 rnorm(10) # ten random numbers from the normal distribution, with a mean of 0 and sd of 1
 
 #' 
 #' ### C. Binomial Distribution
-#' This generates the random number of successes of independent trials. The general form of the function is:\
-#' rbinom(n, size, prob) \
-#' Example: an unbiased coin was tossed 100 times in a row, once a day for a week with a probability that each toss is a success (say, heads)
+#' This generates the random number of successes of independent trials. The general form of the 
+#' ...function is:
+#'          rbinom(n, size, prob)
+#'          
+#' Example: an unbiased coin was tossed 100 times in a row, once a day for a week with a 
+#' ...probability that each toss is a success (i.e., heads)
 #' 
-## ------------------------------------------------------------------------------------------------
 rbinom(n = 7, size = 100, prob = 0.5) # this outputs the number of successes for each trial(n=7)
 
 #' 
 #' 
+## -----------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 #' ## 2. Using custom-made selections
-## ------------------------------------------------------------------------------------------------
-set.seed(3) # this can also be used with the previous functions -- ensures the same set of number is produced each time
+set.seed(3) # this can also be used with the previous functions -- ensures the same set of 
+#...numbers is produced each time
 sample(1:10, 5) # from 1 to 10, select 5 random numbers
 # NB: this is different from runif(5,1,10), whose output would include decimals
 
 #' Alternatively:
-## ------------------------------------------------------------------------------------------------
 population <- 1:10
 sample(population, 5)
 
 #' 
 #' NB - the vector does not have to be sequential, i.e., could be any collection of numbers
-## ------------------------------------------------------------------------------------------------
 pop <- c(30, 10, 50, 20, 70, 90, 40, 100, 80, 60)
 sample(pop, 5)
 
 #' 
 #' It could also be a collection of strings/letters:
-## ------------------------------------------------------------------------------------------------
 popn <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
 sample(popn, 5)
 
 #' 
-#' When the sample specified is larger than the pop, R returns an error because the replace argument is defaulted to "False". To avoid the error message, set replace to "TRUE".
-## ------------------------------------------------------------------------------------------------
+#' When the sample specified is larger than the pop, R returns an error because the replace 
+#' ...argument is defaulted to "False". To avoid the error message, set replace to "TRUE".
 sample(popn, 15, replace = TRUE)
 
 #' 
 #' Another example:
-## ------------------------------------------------------------------------------------------------
 options <- c("heads", "tails")
-sample(options, 10, replace = TRUE) # there are only two elements ("heads"  and "tails"). But the function asks for 10 drawings. Hence, the replace argument is set to "TRUE."
+sample(options, 10, replace = TRUE) # there are only two elements ("heads"  and "tails"). But 
+#...the function asks for 10 drawings. Hence, the replace argument is set to "TRUE."
 
 #' 
-#' 
+#' -------END OF SCRIPT -------
 #' 
 #' 
