b5285ed00eaaa3a0f1d97ece8ad5ee7f3f52821c
tohajan <tohajanlillah@gmail.com>
2024-09-14 08:24:56 -0400

    upload a file

---
+++ 1. Intro.R
@@ -0,0 +1,5 @@
+#---
+#------------------------------ Basic commands in R -------------------------
+c(5, 7, 9, 8) #concatenates a series of items/objects
+?apply #checks what a function is about (opens a new window about the function)
+x <- c(9.0, 5.2, 7.1) # this syntax allows a function (right hand side-RHS) to be saved into an object (LHS; e.g., var name, etc), and thus usable for future operations
